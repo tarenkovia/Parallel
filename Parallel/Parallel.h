eced0eb4fde23293d6da4d28ebcd3deb23674a40
tarenkovia <tarenkov_ivan_00@mail.ru>
2023-11-20 23:17:14 +0400

    add 7-8 lab

--- Parallel/Parallel.h
+++ Parallel/Parallel.h
@@ -9,7 +9,7 @@
 #include <iostream>
 #include "Task1.h"
 #include "Task1_1.h"
-#define NNN 100
+//#define NNN 100
 
 //int main()
 //{
@@ -339,101 +339,170 @@
 //}
 
 // TASK 7 - numb 5
-#pragma once //Concurrent_Simps.h
-#include <ppl.h>
-#define _USE_MATH_DEFINES //Simps_Tst.cpp
-#include <iostream>
+//#pragma once //Concurrent_Simps.h
+//#include <ppl.h>
+//#define _USE_MATH_DEFINES //Simps_Tst.cpp
+//#include <iostream>
+//#include <cmath>
+//#include <time.h>
+//#include <locale.h>
+//using namespace std;
+//using namespace concurrency;
+//typedef double (*Double_Func_Double)(double);
+//double Simps(double a, double b, int N, Double_Func_Double);
+//double Concurrent_Simps(double a, double b, int N, Double_Func_Double);
+//namespace MethodCall {
+//	static void* ObjAddr = nullptr; //без __declspec(thread)
+//	template <class Ty>
+//	double Sub_Int_Func(double x)
+//	{
+//		return (*((Ty*)ObjAddr))(x);
+//	}
+//	template <class Ty>
+//	double Simpson(double a, double b, int N, Ty const& Obj)
+//	{
+//		ObjAddr = (void*)&Obj;
+//		return Simps(a, b, N, Sub_Int_Func<Ty>);
+//	}
+//	template <class Ty>
+//	double Concurrent_Simpson(double a, double b, int N, Ty const& Obj)
+//	{
+//		ObjAddr = (void*)&Obj;
+//		return Concurrent_Simps(a, b, N, Sub_Int_Func<Ty>);
+//	}
+//};
+//
+//double Simps(double a, double b, int N, Double_Func_Double Func)
+//{
+//	double h = (b - a) / (2 * N);
+//	double S1 = 0, S2 = 0;
+//	for (int k = 1; k < N; k++) {
+//		double Tmp = a + (2 * k - 1) * h; S1 += Func(Tmp); S2 += Func(Tmp + h);
+//	}
+//	S1 += Func(b - h); return h * (Func(a) + Func(b) + 4.0 * S1 + 2.0 * S2) / 3.0;
+//}
+//double Concurrent_Simps(double a, double b, int N, Double_Func_Double Func)
+//{
+//	double h = (b - a) / (2 * N);
+//	combinable<double> CS1([]() {return 0.0; }), CS2([]() { return 0.0; });
+//	parallel_for(1, N, [a, h, Func, &CS1, &CS2](int k)
+//		{double Tmp = a + (2 * k - 1) * h;
+//	CS1.local() += Func(Tmp); CS2.local() += Func(Tmp + h); });
+//	double S1 = CS1.combine([](double x, double y) {return x + y; });
+//	double S2 = CS2.combine([](double x, double y) {return x + y; });
+//	S1 += Func(b - h); return h * (Func(a) + Func(b) + 4.0 * S1 + 2.0 * S2) / 3.0;
+//}
+//
+//class My_Class {
+//private:
+//	double y;
+//	const int N = 200000; //Число разбиений отрезка интегрир.
+//	const double _Inf = 5000.0; //Факт. верхний предел интегрир.
+//public:
+//	My_Class(double _y = 0.0) { y = _y; }
+//	double Sub_Int_Func(double x)
+//	{
+//		double Tmp = 15 * log(10.0) + log(abs(x)) - sqrt(x);
+//		int N = Tmp > 0 ? ceil(Tmp * Tmp + 1) : 1;
+//		double P = exp(-abs(x));
+//		double Tmp2 = pow(cos(y), 2);
+//		for (int k = 0; k <= N; k++)
+//			P *= cos(x / (Tmp2 + exp(sqrt((double)k))));
+//		return P;
+//	}
+//	double Quad()
+//	{
+//		return MethodCall::Simpson(0.0, _Inf, N,
+//			[this](double x) {return Sub_Int_Func(x); });
+//	}
+//	double Concurrent_Quad()
+//	{
+//		return MethodCall::Concurrent_Simpson(0.0, _Inf, N,
+//			[this](double x) {return Sub_Int_Func(x); });
+//	}
+//};
+//int main(void)
+//{
+//	setlocale(LC_ALL, ".ACP");
+//	double y; cout << "y="; cin >> y;
+//	My_Class TObj(y);
+//	double Tms = clock();
+//	double F = TObj.Quad();
+//	Tms = (clock() - Tms) / CLOCKS_PER_SEC;
+//	cout.precision(8);
+//	cout << "F=" << F << endl << "Время=" << Tms << " с" << endl;
+//	Tms = clock();
+//	F = TObj.Concurrent_Quad();
+//	Tms = (clock() - Tms) / CLOCKS_PER_SEC;
+//	cout << "F=" << F << endl << "Время=" << Tms << " с" << endl;
+//}
+
+#include <agents.h> // conveyor-transformer.cpp
 #include <cmath>
-#include <time.h>
+#include <iostream>
 #include <locale.h>
-using namespace std;
+#include <time.h>
 using namespace concurrency;
-typedef double (*Double_Func_Double)(double);
-double Simps(double a, double b, int N, Double_Func_Double);
-double Concurrent_Simps(double a, double b, int N, Double_Func_Double);
-namespace MethodCall {
-	static void* ObjAddr = nullptr; //без __declspec(thread)
-	template <class Ty>
-	double Sub_Int_Func(double x)
-	{
-		return (*((Ty*)ObjAddr))(x);
-	}
-	template <class Ty>
-	double Simpson(double a, double b, int N, Ty const& Obj)
-	{
-		ObjAddr = (void*)&Obj;
-		return Simps(a, b, N, Sub_Int_Func<Ty>);
-	}
-	template <class Ty>
-	double Concurrent_Simpson(double a, double b, int N, Ty const& Obj)
-	{
-		ObjAddr = (void*)&Obj;
-		return Concurrent_Simps(a, b, N, Sub_Int_Func<Ty>);
-	}
-};
+using namespace std;
+int const N1 = 100; //Размерность рядов
 
-double Simps(double a, double b, int N, Double_Func_Double Func)
+int const Chunk = 100; //Порция обрабатываемых элементов
+double Func1(double x)//Первая ступень конвейера
 {
-	double h = (b - a) / (2 * N);
-	double S1 = 0, S2 = 0;
-	for (int k = 1; k < N; k++) {
-		double Tmp = a + (2 * k - 1) * h; S1 += Func(Tmp); S2 += Func(Tmp + h);
+	double Tmp = 0, Tmpa = x * x;
+	for (int n = 0; n <= N1; n++) {
+		double Tmpxn = x * x + n, Tmpn3 = n * n * n;
+		for (int k = 0; k <= N1; k++) {
+			double Tmpk = k * k;
+			for (int j = 0; j <= N1; j++)
+				Tmp += Tmpxn / (1 + Tmpa + Tmpn3 + k * k + j * j);
+		}
 	}
-	S1 += Func(b - h); return h * (Func(a) + Func(b) + 4.0 * S1 + 2.0 * S2) / 3.0;
-}
-double Concurrent_Simps(double a, double b, int N, Double_Func_Double Func)
-{
-	double h = (b - a) / (2 * N);
-	combinable<double> CS1([]() {return 0.0; }), CS2([]() { return 0.0; });
-	parallel_for(1, N, [a, h, Func, &CS1, &CS2](int k)
-		{double Tmp = a + (2 * k - 1) * h;
-	CS1.local() += Func(Tmp); CS2.local() += Func(Tmp + h); });
-	double S1 = CS1.combine([](double x, double y) {return x + y; });
-	double S2 = CS2.combine([](double x, double y) {return x + y; });
-	S1 += Func(b - h); return h * (Func(a) + Func(b) + 4.0 * S1 + 2.0 * S2) / 3.0;
+	return Tmp;
 }
 
-class My_Class {
-private:
-	double y;
-	const int N = 200000; //Число разбиений отрезка интегрир.
-	const double _Inf = 5000.0; //Факт. верхний предел интегрир.
-public:
-	My_Class(double _y = 0.0) { y = _y; }
-	double Sub_Int_Func(double x)
-	{
-		double Tmp = 15 * log(10.0) + log(abs(x)) - sqrt(x);
-		int N = Tmp > 0 ? ceil(Tmp * Tmp + 1) : 1;
-		double P = exp(-abs(x));
-		double Tmp2 = pow(cos(y), 2);
-		for (int k = 0; k <= N; k++)
-			P *= cos(x / (Tmp2 + exp(sqrt((double)k))));
-		return P;
-	}
-	double Quad()
-	{
-		return MethodCall::Simpson(0.0, _Inf, N,
-			[this](double x) {return Sub_Int_Func(x); });
-	}
-	double Concurrent_Quad()
-	{
-		return MethodCall::Concurrent_Simpson(0.0, _Inf, N,
-			[this](double x) {return Sub_Int_Func(x); });
-	}
-};
-int main(void)
+double Func2(double x)//Вторая ступень конвейера
 {
+	double Tmp = 0, Tmpa = x * x;
+	for (int n = 0; n <= N1; n++) {
+		double Tmpn = n * n;
+		for (int k = 0; k <= N1; k++) {
+			double Tmpk = k * k * k;
+			for (int j = 0; j <= N1; j++)
+				Tmp += 1.0 / (1 + Tmpa + pow((Tmpn + j * j), 3 / 2) + Tmpk);
+		}
+	}
+	return x * x * Tmp;
+}
+
+int main()
+{
+	int const NNN = 1000; //Длина массива на входе конвейера
 	setlocale(LC_ALL, ".ACP");
-	double y; cout << "y="; cin >> y;
-	My_Class TObj(y);
+	vector<double> X(NNN), Y(NNN), Z(NNN);
+	for (int k = 0; k < X.size(); k++)//Заполнение входного вектора
+		X[k] = 10 * sin((double)k);
 	double Tms = clock();
-	double F = TObj.Quad();
+	for (int k = 0; k < X.size(); k++) {//Последоват. вычисления
+		Y[k] = Func1(X[k]); Y[k] = Func2(Y[k]);
+	}
+
 	Tms = (clock() - Tms) / CLOCKS_PER_SEC;
-	cout.precision(8);
-	cout << "F=" << F << endl << "Время=" << Tms << " с" << endl;
+	cout << "Время последовательного алгоритма: " << Tms << " c." << endl;
+	transformer<double, double> t1(Func1), //Ступени конвейера
+		t2(Func2);
+	t1.link_target(&t2); // t1 -> t2
 	Tms = clock();
-	F = TObj.Concurrent_Quad();
+	for (int k = 0; k < Chunk; k++) //Параллельные вычисления
+		send(t1, X[k]);
+	for (int k = Chunk; k < X.size(); k++) {
+		Z[k - Chunk] = receive(t2); send(t1, X[k]);
+	}
+	for (int k = X.size() - Chunk; k < X.size(); k++) {
+		Z[k] = receive(t2);
+	}
 	Tms = (clock() - Tms) / CLOCKS_PER_SEC;
-	cout << "F=" << F << endl << "Время=" << Tms << " с" << endl;
+	cout << "Время параллельного алгоритма: " << Tms << " c."
+		<< endl;
 }
 
